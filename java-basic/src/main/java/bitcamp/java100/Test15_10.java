fce23c7850576c10cc2109c9c85421c3af5d87b2
dazzul94 <dazzul@naver.com>
2017-10-11 11:39:46 +0900

    d

--- java-basic/src/main/java/bitcamp/java100/Test15_10.java
+++ java-basic/src/main/java/bitcamp/java100/Test15_10.java
@@ -3,7 +3,7 @@ public class Test15_10{
     public static void main(String[] args) {
     //비트연산자(& ,| ,~ , ^)
         int v1 = 0b0101_1100;
-        int v2 = 0b0110_0110;
+        int v2 = 0b0110_0110; 
         
         
         //&(논리곱 AND)
