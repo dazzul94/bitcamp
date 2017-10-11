4f700bce7bf73114a17dc1d372f63dba76794994
dazzul94 <dazzul@naver.com>
2017-10-11 11:08:37 +0900

    d

--- java-basic/src/main/java/bitcamp/java100/Test15_8.java
+++ java-basic/src/main/java/bitcamp/java100/Test15_8.java
@@ -14,7 +14,7 @@ public class Test15_8{
     System.out.printf("false || true = %b\n", false || true); 
     System.out.printf("false || false = %b\n", false || false);
 
-//한개 쓸때
+//한개 쓸때 : 두개 쓸때랑 결과는 같다
     System.out.printf("true & true = %b\n", true & true); 
     System.out.printf("true & false = %b\n", true & false); 
     System.out.printf("false & true = %b\n", false & true); 
@@ -25,13 +25,25 @@ public class Test15_8{
     System.out.printf("false | true = %b\n", false | true); 
     System.out.printf("false | false = %b\n", false | false);
 
-    // 차이점
+    //  &&와 & 차이점
     boolean b1 = false;
     boolean result = false && (b1 = true);
     System.out.printf("b1=%b, result=%b\n",b1, result);
-
+    //&&는 앞에 false 면 뒤에꺼보지도 않음 
+    
     b1 = false;
     result = false & (b1 =true);
     System.out.printf("b1=%b, result=%b\n",b1, result);
+    //혼자 하니까 꾸역꾸역다함
+    
+    // ||와 | 차이점
+    b1 = false;
+    result = true || (b1 = true);
+    System.out.printf("b1=%b, result=%b\n",b1, result);
+    
+    b1 = false;
+    result = true | (b1 = true);
+    System.out.printf("b1=%b, result=%b\n",b1, result);
+    
     }
 }
