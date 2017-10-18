d57a9219749881a65633672410a8eb3deb8f557c
dazzul94 <dazzul@naver.com>
2017-10-18 19:30:08 +0900

    d

--- java-basic/src/main/java/bitcamp/java100/Test21_5.java
+++ java-basic/src/main/java/bitcamp/java100/Test21_5.java
@@ -5,25 +5,25 @@ import java.io.Console;
 public class Test21_5 {
     
     public static void main(String[] args) {
-//       while (true) { 
-           
-           Console console = System.console();
-           
-           if (console == null) {
-           System.err.println("콘솔을 지원하지 않습니다");
-           System.exit(1); //JVM을 종료한다
-           
-           String fuck = console.readLine("구구단?");
-//           break;
-//           for (int x = i, y = 1;y <= 9;y++) {
-//               System.out.printf("%d * %d = %d\n", x, y, x * y);
-//               if(i2 == 10) {
-//                   System.out.println("2에서 9단까지만 가능합니다");
-//               } else if(i2 == 1) { 
-//                   System.out.println("2에서 9단까지만 가능합니다");
-//               } else if(i2 == 0 || i2 < 0)
-//                   System.out.println("다음에 또 봐요!");
-
-           }
-    }
+        
+        Console console = System.console();
+        if (console == null) {
+            System.err.println("콘솔을 지원하지 않습니다");
+            System.exit(1); //JVM을 종료한다
+        }
+        int gugu = Integer.parseInt(console.readLine("구구단? "));
+        
+        for (int x = gugu, y = 1; y <= 9; y++) {
+            if (x <= 9 & x >= 2) {  
+                System.out.printf("%d * %d = %d\n", x, y, x * y);
+            } else if (x == 1) { 
+                System.out.println("2에서 9단까지만 가능합니다!");
+            } else if (x == 10) {
+                System.out.println("2에서 9단까지만 가능합니다!");
+            } else if (x == 0) {
+                System.out.println("다음에 또 봐요!");
+            }
+        }
+        
+    }      
 }
