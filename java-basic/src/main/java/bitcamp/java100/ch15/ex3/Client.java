3c210d95d440712dde37a177489a506f924d9985
dazzul94 <dazzul@naver.com>
2017-11-15 16:44:54 +0900

    .

---
+++ java-basic/src/main/java/bitcamp/java100/ch15/ex3/Client.java
@@ -0,0 +1,27 @@
+package bitcamp.java100.ch15.ex3;
+
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) throws Exception {
+        Socket socket = new Socket("localhost", 9999); //블로킹됌 알아서
+        System.out.println("서버와 연결되었다");
+        
+        PrintStream out = new PrintStream(socket.getOutputStream());
+        
+        out.println("hello");
+        
+        Scanner in = new Scanner(socket.getInputStream());
+        
+        String line = in.nextLine();
+        System.out.println(line);
+        
+        out.close();
+        in.close();
+        
+        socket.close();
+    }
+
+}
