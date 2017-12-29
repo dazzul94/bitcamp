2b0194f206cd54e58f848364045a481c4542b02e
dazzul94 <dazzul94@naver.com>
2017-12-29 13:04:19 +0900

    .

---
+++ mybatis/src/main/java/step9/MybatisInsert.java
@@ -0,0 +1,18 @@
+package step9;
+
+public class MybatisInsert {
+    public static void main(String[] args) throws Exception {
+        
+        BoardDao boardDao = new BoardDao();
+        boardDao.setSqlSessionFactory(SqlSessionFactoryBean.getObject());
+        
+        // insert를 실행할 때 Board 객체를 넘겨줄 것이다.
+        Board board = new Board();
+        board.setTitle("오호라~ 제목이래요213213!");
+        board.setContents("내용이더래요~~~2");
+        
+        int count = boardDao.insert(board);
+        
+        System.out.printf("%d 개가 입력되었습니다.", count);
+    }
+}
